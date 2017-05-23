36ec8c639614642f067447bc3ad0e2a1eb7467f9
Ben Lau <xbenlau@gmail.com>
2017-05-23 12:58:21 +0800

    Add test_qml_loading test

--- qt/templates/MultipleTargetProject/tests/MYPROJECTunittests/tests.cpp
+++ qt/templates/MultipleTargetProject/tests/MYPROJECTunittests/tests.cpp
@@ -1,6 +1,7 @@
 #include <QQmlApplicationEngine>
 #include <QTest>
 #include <Automator>
+#include <QtShell>
 #include "tests.h"
 
 Tests::Tests(QObject *parent) : QObject(parent)
@@ -11,8 +12,38 @@ Tests::Tests(QObject *parent) : QObject(parent)
     Q_UNUSED(ref);
 }
 
-void Tests::testCase()
-{
+void Tests::test_qml_loading()
+{    
+    QFETCH(QString, input);
 
+    QQmlEngine engine;
+    engine.addImportPath("qrc:///");
+
+
+    QQmlComponent comp(&engine);
+    comp.loadUrl(QUrl(input));
+
+    if (comp.isError()) {
+        qDebug() << QString("%1 : Load Failed. Reason :  %2").arg(input).arg(comp.errorString());
+    }
+    QVERIFY(!comp.isError());
+}
+
+void Tests::test_qml_loading_data()
+{
+    QTest::addColumn<QString>("input");
+    QStringList files;
+    files << QtShell::find(QtShell::realpath_strip(SRCDIR,"../../app/MYTARGET"), "*.qml");
+
+    foreach (QString file , files) {
+        QString content = QtShell::cat(file);
+        content = content.toLower();
+
+        if (content.indexOf("pragma singleton") != -1) {
+            continue;
+        }
+
+        QTest::newRow(QtShell::basename(file).toLocal8Bit().constData()) << file;
+    }
 }
 
