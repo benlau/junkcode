fec1af28e8049e9a518e0987399f9ba8f551a90e
Ben Lau <xbenlau@gmail.com>
2017-10-06 00:13:39 +0800

    generate defaultWizard

--- qt/qtcwizard/init.js
+++ qt/qtcwizard/init.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var shell = require('shelljs');
 const path = require('path');
+var defaultWizardJson = require("./default-wizard.js");
 
 // Default rule
 var defaultRule = {
@@ -26,7 +27,15 @@ function run(output) {
     if (!shell.test("-e", ruleFilePath)) {
         shell.ShellString(JSON.stringify(defaultRule,null,4)).to(ruleFilePath);
         console.log("rule.json created");
-    }    
+    }
+    
+    var wizardFilePath = path.resolve(output, "wizard.json");
+    
+    if (!shell.test("-e", wizardFilePath)) {
+        shell.ShellString(JSON.stringify(defaultWizardJson,null,4)).to(wizardFilePath);
+        console.log("wizard.json created");
+    }
+    
 }
 
 run(shell.pwd().toString());
