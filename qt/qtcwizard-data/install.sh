aa94b72dab0b2dd8ecfc1473c7725fb68e2b7046
Ben Lau <xbenlau@gmail.com>
2017-10-07 14:02:55 +0800

    Commit missing files

---
+++ qt/qtcwizard-data/install.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cd project1; node ../../qtcwizard/qtcwizard.js generate ~/.config/QtProject/qtcreator/templates/wizards/wizard-project1
+open ~/.config/QtProject/qtcreator/templates/wizards/wizard-project1
