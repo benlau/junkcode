28671aeec1d27f4c75ad25d17d1b7d2509f7225e
Ben Lau <xbenlau@gmail.com>
2017-10-07 17:02:53 +0800

    restructure folder structure

--- qt/qtcwizard-data/test.sh
+++
@@ -1,6 +0,0 @@
-#/bin/sh
-
-rm -rf tmp
-(cd project1; node ../../qtcwizard/qtcwizard.js generate ../tmp)
-diff -urN wizard-project1 tmp
-
---
+++ qtcwizard-project/install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+cd project1; node ../qtcwizard/qtcwizard.js install
+
+open ~/.config/QtProject/qtcreator/templates/wizards
