31caae15cbb1c2a1074c1158fd1d916dda97f6e8
Ben Lau <xbenlau@gmail.com>
2018-04-18 15:47:08 +0800

    qmake_extra set QML_IMPORT_PATH if qml folder is present

--- conan/qmake_extra/test_package/conanfile.py
+++ conan/qmake_extra/test_package/conanfile.py
@@ -3,7 +3,8 @@ import os
 
 class QuickpromiseTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "qmake"
+    generators = ["qmake", "qmake_extra"]
+    requires = "QmakeExtra/0.1@demo/testing", "QuickPromise/1.0.8@demo/testing"
 
     def build(self):
         print(self.source_folder)
---
+++ conan/qmake_extra/test_package/main.cpp
@@ -0,0 +1,11 @@
+
+#include <QApplication>
+#include <QStringList>
+#include <QtQuickTest/quicktest.h>
+#include <QtCore>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    return 0;
+}
