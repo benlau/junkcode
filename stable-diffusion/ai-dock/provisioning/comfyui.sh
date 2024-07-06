e70dfec3a38400c954b598ae051811506036e0fb
Ben Lau <xbenlau@gmail.com>
2024-07-06 16:19:30 +0800

    Update comfyui.sh

--- stable-diffusion/ai-dock/provisioning/comfyui.sh
+++ stable-diffusion/ai-dock/provisioning/comfyui.sh
@@ -5,6 +5,7 @@
 DEFAULT_PROVISIONING_SCRIPT="https://raw.githubusercontent.com/ai-dock/comfyui/main/config/provisioning/default.sh"
 
 function run_provisioning_script() {
+    echo ubw: Run workspace provisioning script
     TARGET="$1"
     SCRIPT="$2"
     curl -L -o ${TARGET} ${SCRIPT}
@@ -18,13 +19,13 @@ function run_provisioning_script() {
 function run_workspace_provisioning_script() {
     for script in `find /workspace -name provisioning.sh`
     do 
-        echo ubw: run ${script}
-        ${script}    
+        echo ubw: sudo run ${script}
+        sudo ${script}    
     done
 }
+
+run_workspace_provisioning_script
 
 echo ubw: provisioning script started
 run_provisioning_script  /tmp/provision.sh ${DEFAULT_PROVISIONING_SCRIPT}
 
-
-run_workspace_provisioning_script
